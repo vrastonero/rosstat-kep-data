59b0cfc846a74e49e4d4ec9efd717722a4f56c78
vrastonero <irbis14@gmail.com>
2015-12-02 17:07:12 +0300

    Fixes #31

--- src/test/test_1.py
+++ src/test/test_1.py
@@ -2,15 +2,23 @@
 """
 """
 # IMPORT ERROR: here and below - cannot do proper import of modules from parent directory 
-from src2.common import test_io
+# from src2.common import test_io ##need to recreate common.pyc
 
 # tests below rely on hardcoded.py
-from ..load_spec import test_individial_docs_and_dicts, test_in_one_doc, test_with_file
-from ..label_csv import test_label_csv1, test_segment_specs, test_label_csv2
+try:
+	from ..load_spec import test_individial_docs_and_dicts, test_in_one_doc, test_with_file
+	from ..label_csv import test_label_csv1, test_segment_specs, test_label_csv2
+	from ..stream import test_flat_emitter
+	from ..database import test_database    
+	# for this test database must be filled with data
+	from ..query import test_get_df_and_ts
+except SystemError:
+	import os
+	os.chdir('..')
+	from load_spec import test_individial_docs_and_dicts, test_in_one_doc, test_with_file
+	from label_csv import test_label_csv1, test_segment_specs, test_label_csv2
+	from stream import test_flat_emitter
+	from database import test_database  
+	#from query import test_get_df_and_ts  
 # ---------------------------------
 
-from ..stream import test_flat_emitter
-from ..database import test_database    
-
-# for this test database must be filled with data
-from ..query import test_get_df_and_ts
